e3486d2c077629c7c7a661e173cd3da9c523b812
vaishnavi-chandrabose <vaishnavisathya4@gmail.com>
2023-09-04 14:10:59 +0530

    init

--- polls/views.py
+++ polls/views.py
@@ -9,7 +9,7 @@ import json
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse("hai.")
 
 def login(request):
     template = 'login.html'
